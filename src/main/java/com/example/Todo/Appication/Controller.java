7c452058efa0f794bf86474462c964a09317e605
rkddnr <rkddnr05@naver.com>
2023-08-07 13:30:11 +0900

    2

--- src/main/java/com/example/Todo/Appication/Controller.java
+++ src/main/java/com/example/Todo/Appication/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    // dev Test
     @GetMapping("/")
     public String HelloTodo(){
         return "To-do Application !";
